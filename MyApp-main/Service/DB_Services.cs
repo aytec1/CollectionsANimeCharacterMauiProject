37025ad9f51788792e5465482ab2230a0cdcb1bc
MattLambot <mat220802@gmail.com>
2025-04-18 12:14:52 +0200

    debut db

---
+++ MyApp-main/Service/DB_Services.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+class Program
+{
+    static void TestMongoConnection()
+    {
+        string connectionString = "mongodb://student:IAmTh3B3st@185.157.245.38:5003";
+
+        var client = new MongoClient(connectionString);
+        var database = client.GetDatabase("Anime");
+        var collection = database.GetCollection<BsonDocument>("AnimeCharacter");
+
+        Console.WriteLine("Connexion réussie à MongoDB!");
+
+        var documents = collection.Find(new BsonDocument()).ToList();
+        foreach (var document in documents)
+        {
+            Console.WriteLine(document.ToString());
+        }
+    }
+}
